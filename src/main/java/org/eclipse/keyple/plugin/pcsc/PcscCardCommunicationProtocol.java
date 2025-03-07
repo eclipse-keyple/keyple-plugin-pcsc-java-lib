5d776c5df41f135e28e8ba496d3fb6f8456a7e27
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2025-03-07 10:50:39 +0100

    docs: improve card protocol documentation formatting

--- src/main/java/org/eclipse/keyple/plugin/pcsc/PcscCardCommunicationProtocol.java
+++ src/main/java/org/eclipse/keyple/plugin/pcsc/PcscCardCommunicationProtocol.java
@@ -24,9 +24,14 @@ package org.eclipse.keyple.plugin.pcsc;
 public enum PcscCardCommunicationProtocol {
 
   /**
-   * Any ISO 14443-4 compliant card or device (both Type A and Type B) According to PC/SC Part 3,
-   * ISO 14443-4 devices start with the following historical bytes: - Type A: 3B8880... or 3B8B80...
-   * - Type B: 3B8C80...
+   * Any ISO 14443-4 compliant card or device (both Type A and Type B).
+   *
+   * <p>According to PC/SC Part 3, ISO 14443-4 devices start with the following historical bytes:
+   *
+   * <ul>
+   *   <li>Type A: 3B8880... or 3B8B80...
+   *   <li>Type B: 3B8C80...
+   * </ul>
    *
    * <p>Default rule = <b>{@code 3B8880.*|3B8B80.*|3B8C80.*}</b>
    *
@@ -35,9 +40,15 @@ public enum PcscCardCommunicationProtocol {
   ISO_14443_4("3B8880.*|3B8B80.*|3B8C80.*"),
 
   /**
-   * Calypso devices using B Prime protocol According to PC/SC Part 3, B Prime cards use a specific
-   * ATR format: - Initial bytes: 3B8F8001805A0 - Historical bytes encoding Calypso data - End
-   * marker: 829000
+   * Calypso cards using Innovatron B Prime protocol.
+   *
+   * <p>According to PC/SC Part 3, B Prime cards use a specific ATR format:
+   *
+   * <ul>
+   *   <li>Initial bytes: 3B8F8001805A0
+   *   <li>Historical bytes encoding Calypso data
+   *   <li>End marker: 829000
+   * </ul>
    *
    * <p>Default rule = <b>{@code 3B8F8001805A0.*.829000}</b>
    *
@@ -46,9 +57,15 @@ public enum PcscCardCommunicationProtocol {
   INNOVATRON_B_PRIME("3B8F8001805A0.*.829000"),
 
   /**
-   * NXP MIFARE Ultralight and UltralightC technologies According to PC/SC Part 3 Supplemental
-   * Document, memory cards include: - Initial bytes: 3B8F8001804F0CA0000003 - Card type ID: 0603
-   * (for memory cards) - Memory card type: 0003 (for Mifare UL/ULC)
+   * NXP MIFARE Ultralight and UltralightC technologies.
+   *
+   * <p>According to PC/SC Part 3 Supplemental Document:
+   *
+   * <ul>
+   *   <li>Initial bytes: 3B8F8001804F0CA0000003
+   *   <li>Card protocol: 0603 (Type A-3)
+   *   <li>Card type: 0003 (for Mifare UL/ULC)
+   * </ul>
    *
    * <p>Default rule = <b>{@code 3B8F8001804F0CA0000003060300030.*}</b>
    *
@@ -57,10 +74,17 @@ public enum PcscCardCommunicationProtocol {
   MIFARE_ULTRALIGHT("3B8F8001804F0CA0000003060300030.*"),
 
   /**
-   * STMicroelectronics ST25 memory tags According to PC/SC Part 3 Supplemental Document: - Initial
-   * bytes: 3B8F8001804F0CA0000003 - Card family: 0607 (ST specific ID) - Card type: 0007 (ST25 tag)
+   * STMicroelectronics ST25 memory tags.
    *
-   * <p>Default rule = <b>{@code 3B8F8001804F0CA000000306070007.*}</b>
+   * <p>According to PC/SC Part 3 Supplemental Document:
+   *
+   * <ul>
+   *   <li>Initial bytes: 3B8F8001804F0CA0000003
+   *   <li>Card protocol: 0605, 0606, 0607 (Type B-1,2 or 3)
+   *   <li>Card type: 0007 (ST25 tag)
+   * </ul>
+   *
+   * <p>Default rule = <b>{@code 3B8F8001804F0CA0000003060(5|6|7)0007.*}</b>
    *
    * @since 2.5.0
    */
