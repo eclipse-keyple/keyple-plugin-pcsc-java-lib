c4dd94402884c6657966317aa313c414387f12b3
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2026-04-28 14:43:15 +0200

    feat: add INNOVATRON_B_PRIME protocol handling and improve card disconnection logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 dependencies {
   implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
   implementation("org.eclipse.keyple:keyple-common-java-api:2.0.2")
-  implementation("org.eclipse.keyple:keyple-plugin-java-api:2.3.2")
+  implementation("org.eclipse.keyple:keyple-plugin-java-api:3.0.0-SNAPSHOT") { isChanging = true }
   implementation("org.eclipse.keyple:keyple-util-java-lib:2.4.1")
   implementation("net.java.dev.jna:jna:5.15.0")
   compileOnly("org.slf4j:slf4j-api:1.7.36")
